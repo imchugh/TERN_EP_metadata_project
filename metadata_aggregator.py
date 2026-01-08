027408f07c3a3afb9233215d3a8e7395b0348a7b
imchugh <imchugh@6600l-221524-l.mobility.unimelb.net.au>
2026-01-08 13:54:47 +1100

    Completed formatting on aggregator. Working product.

--- metadata_aggregator.py
+++ metadata_aggregator.py
@@ -24,7 +24,11 @@ import gsheets_api_client as gsac
 ###############################################################################
 
 CONFIGS_FILE = 'configs.json'
-TARGET_FOLDER_ID = '1sSdrC4ukE52MzHQWQ9fD5SSA3_25_tbH'
+INPUT_FOLDER_ID = '1sSdrC4ukE52MzHQWQ9fD5SSA3_25_tbH'
+OUTPUT_FOLDER_ID = '1rU6jZbNzOLBStdZIEH-twN2ZhKdbFAbm'
+PIXEL_PAD = {'mult': 7, 'offset': 10}
+LONG_COLS = ['description', 'Site history', 'purpose', 'vegetation']
+LONG_COL_SIZE_PXL = 400
 
 with open(CONFIGS_FILE) as f:
     CONFIGS = json.load(f)
@@ -38,6 +42,9 @@ with open(CONFIGS_FILE) as f:
 ### BEGIN FUNCTIONS ###
 ###############################################################################
 
+# -----------------------------------------------------------------------------
+
+
 def aggregate_sheets():
     """
     Get the content of the site-specific google sheets in the directory and
@@ -49,7 +56,7 @@ def aggregate_sheets():
     """
 
     # Get the list of spreadsheets
-    files = gsac.get_sheet_ids(folder_id=TARGET_FOLDER_ID)
+    files = gsac.get_sheet_ids(folder_id=INPUT_FOLDER_ID)
 
     # Get the file content for each
     df_list = []
@@ -68,12 +75,91 @@ def aggregate_sheets():
         )
 
     # Concatenate all results, order, format and rename to DSA names
-    df = pd.concat(df_list, axis=1).fillna('')
+    df = (
+        pd.concat(df_list, axis=1)
+        .fillna('')
+        .replace('<br/>', '\n')
+        .rename_axis(None)
+        )
     df = df[sorted(df.columns)]
-    df = df.rename(
-        {value: key for key, value in CONFIGS['FIELD_ALIASES'].items()}
-    )
+    df = df.T.rename_axis('Site name')
+    rename_dict = {
+        value: key for key, value in CONFIGS['FIELD_ALIASES'].items()
+        }
+    df = df.reset_index().rename(rename_dict, axis=1).set_index(keys='name')
     return df
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+
+
+def aggregate_mod_time():
+
+    # Get the list of spreadsheets
+    files = gsac.get_sheet_ids(folder_id=INPUT_FOLDER_ID)
+
+    # Get the mod info
+    mod_dict = {}
+    for file in files:
+        mod_info = gsac.get_sheet_mod_metadata(sheet_id=file['id'])
+        site = mod_info.pop('name')
+        mod_info.pop('id')
+        mod_dict[site] = mod_info
+
+    return mod_dict
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+
+
+def write_aggregated_sheet():
+
+    df = aggregate_sheets()
+    df = df.reset_index()
+
+    sheet_id = gsac.create_sheet(
+        file_name='aggregated_site_metadata',
+        folder_id=OUTPUT_FOLDER_ID
+        )
+
+    max_len = df.iloc[:, 0].astype(str).map(len).max()
+    max_len = max_len * PIXEL_PAD['mult'] + PIXEL_PAD['offset']
+    tab_formatter = [
+        gsac._get_col_width_formatter(
+            tab_id=0, start=0, end=len(df.columns) + 1, n_px=max_len
+            )
+        ]
+    tab_formatter += [
+        gsac._get_col_width_formatter(
+            tab_id=0,
+            start=df.columns.get_loc(col),
+            end=df.columns.get_loc(col) + 1,
+            n_px=LONG_COL_SIZE_PXL
+            )
+        for col in LONG_COLS
+        ]
+    tab_formatter += [
+        gsac._get_wrap_text_formatter(
+            tab_id=0
+            ),
+        gsac._get_bold_formatter(
+            tab_id=0, start_row_idx=0, end_row_idx=1
+            )
+        ]
+
+    # Do batch update
+    gsac.do_batch_update(
+        sheet_id=sheet_id,
+        body={'requests': tab_formatter}
+    )
+
+    # Write data
+    gsac.write_info_to_sheet(
+        sheet_id=sheet_id,
+        tab_name='Sheet1',
+        data=df
+        )
+# -----------------------------------------------------------------------------
 
 ###############################################################################
 ### END FUNCTIONS ###
